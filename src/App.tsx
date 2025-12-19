4310f30be65babf0ccd1b531ae7e14e8168f5725
philip131200 <philipschubert00@gmail.com>
2025-12-19 10:12:50 +0100

    added css stuff and new subcomponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [showAlert, setShowAlert] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log("Selected item:", item);
+  };
 
   return (
     <div>
-      {showAlert && (
-        <Alert onClose={() => setShowAlert(false)}>Alert message!</Alert>
-      )}
-      <Button onClick={() => setShowAlert(true)}>Show Alert</Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
